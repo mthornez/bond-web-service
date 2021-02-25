d4bdad0f22033a6fb05f4f6f2d609baf4b0cbed9
mthornez <mthorne@sea.yearup.org>
2021-02-24 16:05:02 -0800

    Adding new js for cocktails

---
+++ data/cocktail-year.js
@@ -0,0 +1,88 @@
+{
+	"title": "Cocktail Sorted By Year",
+	"year":[
+              {
+				"Name":"Martini",
+                "Alcohol":"Gin",
+                "Year":1863,
+                "Inventor":"Jerry Thomas",
+                "IngredientList":"Vermouth",
+                "Image":"martini.jpg",
+			  },
+              {
+				"Name":"Manhattan",
+                "Alcohol":"Whiskey",
+                "Year":1874,
+                "Inventor":"Dr. Iain Marshall",
+                "IngredientList":"Sweet Vermouth and Bitters",
+                "Image":"manhattan.jpg",
+			  },
+              {
+				"Name":"Aviation",
+                "Alcohol":"Gin",
+                "Year":1916,
+                "Inventor":"Hugo Ensslin",
+                "IngredientList":"Maraschino Liqueur, Crème De Violette, and Lemon Juice",
+                "Image":"aviation.jpg",
+			  },
+              {
+				"Name":"Bloody Mary",
+                "Alcohol":"Vodka",
+                "Year":1920,
+                "Inventor":"Fernand 'Pete' Petiot",
+                "IngredientList":"Tomato Juice, Worcestershire Sauce, Hot Sauces, Garlic, Herbs, Horseradish, Celery, Olives, Salt, Black Pepper, Lemon Juice, Lime Juice and Celery Salt",
+                "Image":"bloodymary.jpg",
+			  },
+              {
+				"Name":"Sidecar",
+                "Alcohol":"Cognac",
+                "Year":1922,
+                "Inventor":"Pat MacGarry",
+                "IngredientList":"Vorange Liqueur and Lemon Juice",
+                "Image":"sidecar.jpg",
+			  },
+              {
+				"Name":"Moscow Mule",
+                "Alcohol":"Vodka",
+                "Year":1941,
+                "Inventor":"John Martin",
+                "IngredientList":"Ginger Beer and Lime Juice",
+                "Image":"moscowmule.jpg",
+			  },
+              {
+				"Name":"Margarita",
+                "Alcohol":"Tequila",
+                "Year":1942,
+                "Inventor":"Carlos 'Danny' Herrera",
+                "IngredientList":"Orange Liqueur and Lime Juice",
+                "Image":"margarita.jpg",
+			  },
+              {
+				"Name":"Irish Coffee",
+                "Alcohol":"Whiskey",
+                "Year":1943,
+                "Inventor":"Chef Joe Sheridan",
+                "IngredientList":"Hot Coffee and Sugar",
+                "Image":"irishcoffee.jpg",
+			  },
+              {
+				"Name":"Long Island Iced Tea",
+                "Alcohol":"Vodka",
+                "Year":1972,
+                "Inventor":"Robert 'Rosebud' Butt",
+                "IngredientList":"Triple Sec, Tequila, Rum, Gin, Cola",
+                "Image":"longislandicedtea.jpg",
+			  },
+              {
+				"Name":"Cosmopolitan",
+                "Alcohol":"Vodka",
+                "Year":1987,
+                "Inventor":"Toby Cecchini",
+                "IngredientList":"Triple Sec, Cranberry Juice, and Lime Juice",
+                "Image":"cosmopolitan.jpg",
+			  },
+            ]
+}
+
+
+              
